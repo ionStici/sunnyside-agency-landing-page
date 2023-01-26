a4053c31038a3e4d51efeb38527ee6813d1847fd
Ion Stici <ionsticidev@gmail.com>
2023-01-26 14:53:59 +0200

    Improve nav bar with transparent black on scroll

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector(".nav");
 const menuBtn = document.querySelector(".menu-icon");
+const header = document.querySelector(".header");
+const ser = document.querySelector(".ser");
 
 menuBtn.addEventListener("click", function () {
     if (nav.classList.contains("closed")) {
@@ -16,3 +18,16 @@ menuBtn.addEventListener("click", function () {
         nav.classList.add("closed");
     }
 });
+
+const coords = ser.getBoundingClientRect();
+const coordsTop = coords.top;
+
+window.addEventListener("scroll", function (e) {
+    if (window.scrollY > coordsTop) {
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    } else if (window.scrollY < coordsTop) {
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+console.log(window.innerWidth);
